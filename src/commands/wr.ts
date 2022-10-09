4c5d0dbdc3983a1a46f19bd0300f0f1379b8ca3b
buczol1 <buczynskiartur6@gmail.com>
2022-10-09 17:26:33 +0200

    zmiana ifa, bo always true był

--- src/commands/wr.ts
+++ src/commands/wr.ts
@@ -23,7 +23,7 @@ module.exports = {
 				await interaction.reply('Przywoływacz nie jest zarejestrowany, użyj /register **nazwa przywoływacza**');
 				console.log(err);
 			}
-			else if(results[0] !== []){
+			else if(results[0].length !== 0){
 				for(let i = 0; i < results.length; i++) {
 					if(results[i].win){
 						wins++;
